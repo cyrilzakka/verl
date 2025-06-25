118346df39710f0f1be6fe99544092cafc3a3a13
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2025-06-25 15:07:44 -0700

    Lock fix

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -24,6 +24,8 @@ from omegaconf import OmegaConf
 
 from verl.trainer.ppo.ray_trainer import RayPPOTrainer
 from verl.trainer.ppo.reward import load_reward_manager
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
 
 
 @hydra.main(config_path="config", config_name="ppo_trainer", version_base=None)
